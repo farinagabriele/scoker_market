210eed10bb0126bcea1731b9adf129098b8affe7
Valerio Savo <46973674+savobully@users.noreply.github.com>
2023-01-04 11:40:15 +0100

    Giocatore

---
+++ giocatore.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Giocatore(
+     Codice_Giocatore char(11) Primary Key,
+     Nome char(40) NOT NULL,
+     Cognome char(40) NOT NULL,
+     Ruolo char(4),
+     Data_Nascita date,
+     Citta_Provenienza char(20),
+)
