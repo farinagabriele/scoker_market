369b17c06fca36ea3db8f18ac31f5dc3ad7e68c5
franciomanno <121463628+franciomanno@users.noreply.github.com>
2023-01-06 18:20:04 +0100

    popolamento_squadra.sql

---
+++ Progetto_Farina_Mercia_Mannoni_Savo/Popolamento/popolamento_squadra.sql
@@ -0,0 +1,45 @@
+insert into squadra values(
+    "10005483728",
+    "AS Roma",
+    1927,
+    "Piazzale Dino Viola"
+    "Roma"
+    "romacares@asroma.it"
+    "0689386000"
+);
+insert into squadra values(
+    "17674763649",
+    "FC Juventus",
+    1897,
+    "Via Druento",
+    "Torino",
+    "store-support@juventus.com",
+    "0114530486"
+);
+insert into squadra values(
+    "06857453654",
+    "AC Milan"
+    1899,
+    "Via Aldo Rossi",
+    "Milano",
+    "hospitality@acmilan.com",
+    "0262284545"
+);
+insert into squadra values(
+    "10547665474",
+    "SSC Napoli",
+    1926,
+    "Via di Castel Volturno",
+    "Napoli",
+    "sponsorelicenze@sscn.it",
+    "0815095344"
+);
+insert into squadra values(
+    "10009890563",
+    "ACF Fiorentina",
+    1926,
+    "Viale Manfredo Fanti",
+    "Firenze",
+    "fiorentinapoint@acffiorentina.it",
+    "055571259"
+);
