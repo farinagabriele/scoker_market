b81217f582134eedf44f4ebd66bdd261a6b4ec0b
Gabriele Farina <105318769+farinagabriele@users.noreply.github.com>
2023-01-07 23:30:25 +0100

    Update gestionecalciomercato.sql
    Database con record inseriti

--- Progetto_Farina_Mercia_Mannoni_Savo/Database/gestionecalciomercato.sql
+++ Progetto_Farina_Mercia_Mannoni_Savo/Database/gestionecalciomercato.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 04, 2023 at 03:36 PM
+-- Generation Time: Jan 07, 2023 at 11:29 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -38,6 +38,17 @@ CREATE TABLE `contratto` (
   `partita_iva` char(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `contratto`
+--
+
+INSERT INTO `contratto` (`codice_contratto`, `data_inizio`, `data_scadenza`, `stipendio_annuale`, `precentuale_possesso`, `numero_maglia`, `codice_giocatore`, `partita_iva`) VALUES
+(1001, '2022-07-01', '2023-06-30', 4000000, 1, 30, 'A324F4536SD', '10005483728'),
+(1002, '2023-01-01', '2023-06-30', 5500000, 0.8, 9, 'B685G0987SS', '17674763649'),
+(1003, '2023-06-01', '2024-06-30', 12000000, 1, 21, 'C554Z4561LK', '06857453654'),
+(1004, '2023-01-01', '2023-06-30', 1500000, 0.2, 7, 'B685G0987SS', '10547665474'),
+(1005, '2019-01-01', '2019-06-30', 500000, 1, 18, 'E934P34125L', '10009890563');
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +63,17 @@ CREATE TABLE `giocatore` (
   `Data_Nascita` date DEFAULT NULL,
   `Citta_Provenienza` char(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `giocatore`
+--
+
+INSERT INTO `giocatore` (`Codice_Giocatore`, `Nome`, `Cognome`, `Ruolo`, `Data_Nascita`, `Citta_Provenienza`) VALUES
+('A324F4536SD', 'Francesco', 'Totti', 'ATT', '1976-09-27', 'Roma'),
+('B685G0987SS', 'Alessandro', 'Del Piero', 'ATT', '1974-11-09', 'Conegliano'),
+('C554Z4561LK', 'Paolo', 'Maldini', 'DC', '1968-06-26', 'Milano'),
+('D101M2369AM', 'Diego Armando', 'Maradona', 'COC', '1960-10-30', 'Lanus'),
+('E934P34125L', 'Sebastien', 'Frey', 'POR', '1980-03-18', 'Thonon-les-Bains');
 
 -- --------------------------------------------------------
 
@@ -68,6 +90,17 @@ CREATE TABLE `squadra` (
   `Email` varchar(40) DEFAULT NULL,
   `Telefono` char(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `squadra`
+--
+
+INSERT INTO `squadra` (`Partita_IVA`, `Denominazione`, `Anno_fondazione`, `Sede_indirizzo`, `Sede_citta`, `Email`, `Telefono`) VALUES
+('06857453654', 'AC Milan', 1899, 'Via Aldo Rossi', 'Milano', 'hospitality@acmilan.com', '0262284545'),
+('10005483728', 'AS Roma', 1927, 'Piazzale Dino Viola', 'Roma', 'romacares@asroma.it', '0689386000'),
+('10009890563', 'ACF Fiorentina', 1926, 'Viale Manfredo Fanti', 'Firenze', 'fiorentinapoint@acffiorentina.it', '055571259'),
+('10547665474', 'SSC Napoli', 1926, 'Via di Castel Volturno', 'Napoli', 'sponsorelicenze@sscn.it', '0815095344'),
+('17674763649', 'FC Juventus', 1897, 'Via Druento', 'Torino', 'store-support@juventus.com', '0114530486');
 
 --
 -- Indexes for dumped tables
